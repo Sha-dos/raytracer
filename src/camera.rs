2bf3b6e7e49f8cd4a634348a5a03e4b3fc105e06
Nolan Peterson <ndpete2007@gmail.com>
2025-09-02 17:00:58 -0700

    abstract camera

---
+++ src/camera.rs
@@ -0,0 +1,74 @@
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use crate::ray::Ray;
+use crate::vector::{Point3, Vector3};
+use anyhow::Result;
+use crate::hittable::HittableList;
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub image_width: i32,
+    image_height: i32,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vector3,
+    pixel_delta_v: Vector3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let image_width = 400;
+
+        // Calculate the image height, and ensure that it's at least 1.
+        let mut image_height = (image_width as f64 / aspect_ratio) as i32;
+        image_height = if image_height < 1 { 1 } else { image_height };
+
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let center = Vector3::new(0.0, 0.0, 0.0);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        let pixel_delta_u = viewport_u / image_width as f64;
+        let pixel_delta_v = viewport_v / image_height as f64;
+
+        // Calculate the location of the upper left pixel.
+        let viewport_upper_left = center
+            - Vector3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        Self {
+            aspect_ratio,
+            image_width,
+            image_height,
+            center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+    
+    pub async fn render(&self, world: HittableList) -> Result<()> {
+        let mut file = File::create("image.ppm").await?;
+
+        file.write(format!("P3\n{} {}\n255\n", &self.image_width, &self.image_height).as_bytes()).await?;
+
+        for j in 0..self.image_height {
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel00_loc + (i as f64 * self.pixel_delta_u) + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.center;
+                let r = Ray::new(self.center, ray_direction);
+
+                let pixel_color = Ray::color(&r, &world);
+                pixel_color.write_color(&mut file).await?;
+            }
+        }
+        
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use tokio::fs::File;
 use tokio::io::{AsyncWriteExt};
 use anyhow::Result;
+use crate::camera::Camera;
 use crate::hittable::HittableList;
 use crate::hittable::sphere::Sphere;
 use crate::ray::Ray;
@@ -12,56 +13,21 @@ mod ray;
 mod vector;
 mod hittable;
 mod interval;
+mod camera;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
-
-    // Calculate the image height, and ensure that it's at least 1.
-    let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    image_height = if image_height < 1 { 1 } else { image_height };
-
-    // World
     let mut world = HittableList::new();
-
-    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
-
-    // Camera
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let camera_center = Vector3::new(0.0, 0.0, 0.0);
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    // Calculate the location of the upper left pixel.
-    let viewport_upper_left = camera_center
-        - Vector3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    let mut file = File::create("image.ppm").await?;
-
-    file.write(format!("P3\n{} {}\n255\n", image_width, image_height).as_bytes()).await?;
-
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let pixel_center = pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let r = Ray::new(camera_center, ray_direction);
-
-            let pixel_color = Ray::color(&r, &world);
-            pixel_color.write_color(&mut file).await?;
-        }
-    }
+    
+    world.add(Box::new(Sphere::new(Point3::new(0., 0., -1.), 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0., -100.5, -1.), 100.)));
+    
+    let mut camera = Camera::new();
+    
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.image_width = 400;
+    
+    camera.render(world).await?;
 
     Ok(())
 }
