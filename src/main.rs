3c9bbae663d433a39136ec79960d7d62b24fa16b
Nolan Peterson <ndpete2007@gmail.com>
2025-09-10 10:23:43 -0700

    change example to use arc, needed for clone

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,11 @@ async fn main() -> Result<()> {
     let material_bubble = Arc::new(Dielectric::new(1. / 1.5));
     let material_right  = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.));
 
-    world.add(Box::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_ground)));
-    world.add(Box::new(Sphere::new(Point3::new( 0.0, 0.0, -1.2), 0.5, material_center)));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0),  0.4, material_bubble)));
-    world.add(Box::new(Sphere::new(Point3::new( 1.0, 0.0, -1.0), 0.5, material_right)));
+    world.add(Arc::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_ground)));
+    world.add(Arc::new(Sphere::new(Point3::new( 0.0, 0.0, -1.2), 0.5, material_center)));
+    world.add(Arc::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    world.add(Arc::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0),  0.4, material_bubble)));
+    world.add(Arc::new(Sphere::new(Point3::new( 1.0, 0.0, -1.0), 0.5, material_right)));
 
     let mut camera = Camera::new();
     
